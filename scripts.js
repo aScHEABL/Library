ba9d9423dffb9d15766f0ccafce5217943c802e8
aScHEABL <natothun@gmail.com>
2022-07-06 13:57:16 +0800

    feat: add evemt listener to add book button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 console.log(`Hello World!`);
 
+const addBookButton_DOM = document.querySelector(`#add-book-button`);
+
+addBookButton_DOM.addEventListener(`click`, () => {
+    
+});
+
 let myLibary = [];
 
 let book = () => {
